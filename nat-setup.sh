c847e0b5ccdfaa8160d41468bcfcca9b9b397bf6
Sean Corkum <seancorkum81@gmail.com>
2025-07-23 21:22:09 -0500

    updates

--- nat-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-/sbin/iptables -F FORWARD
-service iptables save
